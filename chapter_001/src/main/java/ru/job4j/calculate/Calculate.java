4415ed1ebf125f9f734ccd7519efab609919e013
Elena Kartashova <kartashova.ee@yandex.ru>
2019-01-24 22:18:11 +0100

    1.3. JavaDoc. Code convention[#108206]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author Elena Kartashova (kartashova.ee@yandex.ru)
+*/
 public class Calculate {
 
+/**
+*
+* Конструктор, вывод первого приветствия в консоль.
+* 
+* @param args - входные аргументы.
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello world!");		
 	}
