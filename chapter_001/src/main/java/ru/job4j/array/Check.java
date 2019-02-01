000ea74400b3ce26d9206e4536c9a04ef3eb9f24
Elena Kartashova <kartashova.ee@yandex.ru>
2019-02-01 14:30:49 +0100

    6.3. Массив заполнен true или false[#108187]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * Класс, проверяющий булев массив на однородность.
+ * @author Elena Kartashova (kartashova.ee@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Check {
+
+    /**
+     * Метод проверки массива на однородность.
+     * @param data исходный булев массив.
+     * @return true в случае однородности и false в случае неоднородности массива.
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = true, first = data[0];
+        for (boolean elem : data) {
+            if (elem != first) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Elena Kartashova (kartashova.ee@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CheckTest {
+
+    /**
+     * Тест для однородного массива с нечётным количеством элементов.
+     */
+    @Test
+    public void whenDataMonoByTrueThenTrueOdd() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Тест для неоднородного массива с нечётным количеством элементов.
+     */
+    @Test
+    public void whenDataNotMonoByTrueThenFalseOdd() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    /**
+     * Тест для однородного массива с чётным количеством элементов.
+     */
+    @Test
+    public void whenDataMonoByFalseThenTrueEven() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, false, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Тест для неоднородного массива с чётным количеством элементов.
+     */
+    @Test
+    public void whenDataNotMonoByTrueThenFalseEven() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
