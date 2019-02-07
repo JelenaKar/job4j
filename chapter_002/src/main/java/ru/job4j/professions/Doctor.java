3946da493ff33bfe37e011fc629c5f0f252cfc19
Elena Kartashova <kartashova.ee@yandex.ru>
2019-02-07 14:29:56 +0100

    1. Реализация профессий в коде[#108259]

--- chapter_002/src/main/java/ru/job4j/professions/Doctor.java
+++ chapter_002/src/main/java/ru/job4j/professions/Doctor.java
@@ -1,7 +1,13 @@
 package ru.job4j.professions;
 
-public class Doctor extends Profession {
-    public void treat(Patient patient) {
+class Diagnose {
 
+}
+
+public class Doctor extends Profession {
+
+    public Diagnose heal(Patient patient) {
+        Diagnose diagnose = new Diagnose();
+        return diagnose;
     }
 }
--- chapter_002/src/main/java/ru/job4j/professions/Profession.java
+++ chapter_002/src/main/java/ru/job4j/professions/Profession.java
@@ -3,4 +3,8 @@ package ru.job4j.professions;
 public class Profession {
     protected String name;
     protected String profession;
+
+    public String getName() {
+        return this.name;
+    }
 }
