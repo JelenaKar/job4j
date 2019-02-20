e0b443d34e8bc486bb894de07b4349a41623d322
Elena Kartashova <kartashova.ee@yandex.ru>
2019-02-20 21:47:47 +0100

    1. Конвертация листа массивов в один лист Integer[#108235]

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,5 +1,7 @@
 package ru.job4j.list;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -11,7 +13,7 @@ import java.util.List;
 public class ConvertList2Array {
 
     /**
-     * Метод, приобразующий список в матрицу с заданным числом строк.
+     * Метод, преобразующий список в матрицу с заданным числом строк.
      * @param list - исходный список.
      * @param rows - заданное число строк.
      * @return список, преобразованный в матрицу.
@@ -29,5 +31,21 @@ public class ConvertList2Array {
             }
         }
         return array;
+    }
+
+    /**
+     * Метод, преобразующий список в матрицу с заданным числом строк.
+     * @param list - исходный список.
+     * @return одномерный список, полученный из двумерного.
+     */
+    public List<Integer> convert(List<int[]> list) {
+        Iterator<int[]> it = list.iterator();
+        List<Integer> result = new ArrayList<>();
+        while (it.hasNext()) {
+            for (int el : it.next()) {
+                result.add(el);
+            }
+        }
+        return result;
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -1,7 +1,11 @@
 package ru.job4j.list;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -29,6 +33,23 @@ public class ConvertList2ArrayTest {
                 {4, 5, 6},
                 {7, 0, 0}
         };
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Тест ситуации для двумерного списка с разным количеством элементов в каждом.
+     */
+    @Test
+    public void when2ArraysWithSize25ThenListWith7Elements() {
+        ConvertList2Array converter = new ConvertList2Array();
+
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4, 5, 6, 7});
+
+        List<Integer> result = converter.convert(list);
+
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
         assertThat(result, is(expect));
     }
 }
