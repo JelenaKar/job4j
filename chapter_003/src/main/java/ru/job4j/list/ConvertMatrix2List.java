a0ea123779637acceb0404c905c6d8631a42788e
Elena Kartashova <kartashova.ee@yandex.ru>
2019-02-20 20:59:42 +0100

    4. Конвертация двумерного массива в ArrayList[#108231]

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,28 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс списка, полученного из матрицы.
+ * @author Elena Kartashova (kartashova.ee@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConvertMatrix2List {
+
+    /**
+     * Метод, приобразующий матрицу в список.
+     * @param input - исходная матрица.
+     * @return список, полученный из матрицы.
+     */
+    public List<Integer> toList(int[][] input) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] rows : input) {
+            for (int cells : rows) {
+                list.add(cells);
+            }
+        }
+        return list;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Elena Kartashova (kartashova.ee@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ConvertMatrix2ListTest {
+
+    /**
+     * Тест преобразования матрицы 3 х 3 в список из 9 элементов.
+     */
+    @Test
+    public void when3on3ArrayThenList9() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
