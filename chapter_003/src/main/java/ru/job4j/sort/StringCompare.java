478e93d390947be3fa5c5ec007d1a3e229fba9d9
Elena Kartashova <kartashova.ee@yandex.ru>
2019-02-22 14:00:01 +0100

    3. Компаратор для строк.[#108220]

---
+++ chapter_003/src/main/java/ru/job4j/sort/StringCompare.java
@@ -0,0 +1,40 @@
+package ru.job4j.sort;
+
+import java.util.Comparator;
+
+/**
+ * Класс-компаратор для строк.
+ * @author Elena Kartashova (kartashova.ee@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class StringCompare implements Comparator<String> {
+
+    /**
+     * Метод, сравнивающий две строки, аналог String.compareTo.
+     * @param left первая строка.
+     * @param right вторая строка.
+     * @return 0 - если строки равны; 1 и -1 - если первая строка лексикографически больше или меньше соответственно.
+     */
+    @Override
+    public int compare(String left, String right) {
+        char[] leftChars = left.toCharArray();
+        char[] rightChars = right.toCharArray();
+        int result = 0;
+        int i = 0;
+        while (i < left.length() && i < right.length()) {
+            result = Character.compare(leftChars[i], rightChars[i]);
+            if (result != 0) {
+                break;
+            }
+            i++;
+        }
+        if (result == 0) {
+            if (left.length() - right.length() != 0) {
+                result = (left.length() - right.length()) / Math.abs(left.length() - right.length());
+            }
+        }
+
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/sort/StringCompareTest.java
@@ -0,0 +1,84 @@
+package ru.job4j.sort;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Elena Kartashova (kartashova.ee@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class StringCompareTest {
+
+    /**
+     * Ситуация, когда две строки равны.
+     */
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rst, is(0));
+    }
+
+    /**
+     * Ситуация, когда первая строка является подстрокой второй.
+     */
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+    /**
+     * Ситуация, когда первая и вторая строки начинаются с разных знаков.
+     */
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    /**
+     * Ситуация, когда первая и вторая строки различаются внутренними знаками.
+     */
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+
+    /**
+     * Ситуация, когда первая и вторая строки различаются внутренними знаками.
+     */
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(rst, lessThan(0));
+    }
+
+}
