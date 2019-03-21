47253c614dc27e4499ad18068d126fcf7f360ff1
Elena Kartashova <kartashova.ee@yandex.ru>
2019-03-21 15:57:34 +0100

    5.3.4. Задан связанный список. Определить цикличность.[#122150]

---
+++ junior/chapter_001_Collection_Pro/src/test/java/ru/job4j/list/LinkedCycledListTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Test.
+ *
+ * @author Elena Kartashova (kartashova.ee@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class LinkedCycledListTest {
+
+    private LinkedCycledList<Integer> list = new LinkedCycledList<>();
+
+    /**
+     * Тестирование ситуации, когда отсутствует зацикленность.
+     */
+    @Test
+    public void whenNoCycledThenFalse() {
+        Node<Integer> first = new Node<>(1);
+        Node<Integer> second = new Node<>(11);
+        Node<Integer> third = new Node<>(17);
+        Node<Integer> fourth = new Node<>(22);
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        assertThat(list.hasCycle(first), is(false));
+    }
+
+    /**
+     * Тестирование ситуации, когда присутствует зацикленность.
+     */
+    @Test
+    public void whenCycledThenTrue() {
+        Node<Integer> first = new Node<>(1);
+        Node<Integer> second = new Node<>(11);
+        Node<Integer> third = new Node<>(17);
+        Node<Integer> fourth = new Node<>(22);
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = third;
+        assertThat(list.hasCycle(first), is(true));
+    }
+
+}
