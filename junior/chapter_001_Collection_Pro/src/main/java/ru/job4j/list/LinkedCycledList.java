380e48c11304f91a75f49f0c0b2a95b09ab1b667
Elena Kartashova <kartashova.ee@yandex.ru>
2019-03-22 12:22:18 +0100

    5.3.4. Задан связанный список. Определить цикличность.[#122150] Исправила на алгоритм Брента.

--- junior/chapter_001_Collection_Pro/src/main/java/ru/job4j/list/LinkedCycledList.java
+++ junior/chapter_001_Collection_Pro/src/main/java/ru/job4j/list/LinkedCycledList.java
@@ -16,29 +16,28 @@ public class LinkedCycledList<E> {
      * @return true если список зациклен и false в противном случае.
      */
     public boolean hasCycle(Node<E> first) {
-        Node<E> current = first.next;
-        Node<E>[] temp = (Node<E>[]) new Node[10];
-        int position = 1;
-        temp[0] = first;
-        boolean result = false;
-        while (current.next != null) {
-            for (Node<E> elem : temp) {
-                if (current.equals(elem)) {
-                    result = true;
-                    break;
-                }
+
+        if (first == null) {
+            return false;
+        }
+
+        Node<E> slow = first, fast = first;
+        int taken = 0, power = 2;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            taken++;
+            if (slow == fast) {
+                return true;
             }
-            if (result) {
-                break;
-            } else {
-                if (position >= temp.length) {
-                    temp = Arrays.copyOf(temp, temp.length * 2);
-                }
-                temp[position++] = current;
-                current = current.next;
+
+            if (taken == power) {
+                taken = 0;
+                power *= 2;
+                slow = fast;
             }
         }
-        return result;
+        return false;
     }
 }
 
