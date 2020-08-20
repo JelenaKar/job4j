6c441504d660299c7f93c3cbac7d737fee513195
Elena Kartashova <kartashova.ee@yandex.ru>
2020-08-20 14:29:36 +0200

    2. Лямбды и шаблон wrapper.[49295#182274]

--- middle/chapter_003_Hibernate/src/main/java/ru/job4j/todolist/Actions.java
+++ middle/chapter_003_Hibernate/src/main/java/ru/job4j/todolist/Actions.java
@@ -1,65 +1,47 @@
 package ru.job4j.todolist;
 
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
-import java.util.ArrayList;
+import org.hibernate.Transaction;
 import java.util.List;
+import java.util.function.Function;
 
 public class Actions {
 
     public static Item add(Item item, SessionFactory sf) {
-        try {
-            Session session = sf.openSession();
-            session.beginTransaction();
+        return tx(sf, session -> {
             session.save(item);
-            session.getTransaction().commit();
-            session.close();
-        } catch (HibernateException he) {
-            he.printStackTrace();
-        }
-        return item;
+            return item;
+        });
     }
 
     public static Item update(Item item, SessionFactory sf) {
-        try {
-            Session session = sf.openSession();
-            session.beginTransaction();
+        return tx(sf, session -> {
             session.update(item);
-            session.getTransaction().commit();
-            session.close();
-        } catch (HibernateException he) {
-            he.printStackTrace();
-        }
-        return item;
+            return item;
+        });
     }
 
     public static Item findById(Integer id, SessionFactory sf) {
-        Item result = null;
-        try {
-            Session session = sf.openSession();
-            session.beginTransaction();
-            result = session.get(Item.class, id);
-            session.getTransaction().commit();
-            session.close();
-        } catch (HibernateException he) {
-            he.printStackTrace();
-        }
-        return result;
+        return tx(sf, session -> session.get(Item.class, id));
     }
 
     public static List<Item> findAll(SessionFactory sf) {
-        List result = new ArrayList<>();
+        return tx(sf, session -> session.createQuery("from ru.job4j.todolist.Item ORDER BY created DESC").list());
+    }
+
+    private static <T> T tx(final SessionFactory sf, final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
         try {
-            Session session = sf.openSession();
-            session.beginTransaction();
-            result = session.createQuery("from ru.job4j.todolist.Item ORDER BY created DESC").list();
-            session.getTransaction().commit();
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
             session.close();
-        } catch (HibernateException he) {
-            he.printStackTrace();
         }
-        return result;
     }
 }
